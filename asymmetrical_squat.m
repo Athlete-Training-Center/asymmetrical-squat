f67dcf0c01e808bf56b6dbd601aecef9c4cc3160
jaeho Yoon <dkssud8150@gmail.com>
2024-07-18 19:24:40 +0900

    update 1. asymmetrical squat.m   - during squat, maintain relative difference   - rmse graph 2. MultiInputGUI.m   - you can input the option for Right, Left   - The option is the value of what target to experiment with

--- asymmetrical_squat.m
+++ asymmetrical_squat.m
@@ -1,70 +1,91 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   asymmetrical squat
+%
+%   ~~
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% reset setting
 clear
-
-ip = '127.0.0.1';
-% ### Connect to QTM
-QCM('connect', ip, 'frameinfo', 'force'); % Connects to QTM and keeps the connection alive.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% figure setting
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figureHandle = figure('CloseRequestFcn', @closeRequest);
-function closeRequest(src, callbackdata)
-    % figure 창을 닫을 때 실행할 코드
-    disp('Figure is being closed.');
-    delete(src); % figure 창 삭제
-end
-
-hold on
-set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 1, 0.96]);
-set(gca,'XTICK',[],'YTick',[])
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % bodyweight setting
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%foot_size = inputdlg("input the your body weight (kg): ");
-bodyweight_kg = '80'; % kg
-bodyweight_kg = str2double(bodyweight_kg);
-if bodyweight_kg <= 0 || bodyweight_kg >= 150
-    disp('bad weight! try again');
-end
+[body_weight_kg, option] = MultiInputGUI('AS');
+option = upper(option);
 
 % gravity acceleration (m/s^2)
-gravity = 9.80665; 
-bodyweight_N = bodyweight_kg * gravity;
+gravity = 9.80665;
+bodyweight_N = body_weight_kg * gravity;
 
-% figure size setting
-xlim=[0 1200]; % 600 mm x 2
-ylim = [0 bodyweight_N * 0.5];
-set(gca, 'xlim',xlim, 'ylim',ylim)
+% Connect to QTM
+ip = '127.0.0.1';
+% Connects to QTM and keeps the connection alive.
+QCM('connect', ip, 'frameinfo', 'force');
 
-centerpoint = [(xlim(1)+xlim(2))/2 (ylim(1)+ylim(2))/2];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% figure setting
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Create a figure window
+figureHandle = figure(1);
+hold on
+% set the figure size
+set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 1, 0.96]);
+% remove ticks from axes
+set(gca,'XTICK',[],'YTick',[])
 
+% setting figure size to real force plate size
+%           600mm      600mm
+%        ---------------------
+%      x↑         ¦           ¦
+%       o → y     ¦           ¦ 400mm
+%       ¦         ¦           ¦ 
+%        ---------------------
+% original coordinate : left end(x = 0) and center
+xlim=[0 1200];
+ylim = [0 100]; % 100 %
+set(gca, 'xlim', xlim, 'ylim', ylim)
+
+% center coordinate for figure size
+centerpoint = [(xlim(1) + xlim(2)) / 2, (ylim(1) + ylim(2)) / 2];
+
+% bar blank between vertical center line and each bar
 margin = 300;
 
-side = randi(2); % 1:left, 2:right
-
-switch side
-    case 1
-        loc_x = [centerpoint(1)-margin 0]; % x1 y
-    case 2
-        loc_x = [centerpoint(1)+margin 0]; % x2 y
+switch option
+    case 'R'
+        loc_x = [centerpoint(1)+margin, ylim(1)]; % x2 y
+    case 'L'
+        loc_x = [centerpoint(1)-margin, ylim(1)]; % x1 y
 end
 
-width = 100; % each bar width
+% each bar width
+width = 100;
+% each bar height
+height = ylim(2) - ylim(1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% draw target line
+%% Perform a target force 10 times at a random rate based on weight
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+target_value = 20; %  20 %
+margin_of_error = 1; %  1 %
+target_range = [target_value - margin_of_error, target_value + margin_of_error];
+
+target_line1 = plot([loc_x(1) - width/2, loc_x(1) + width/2], [target_range(1) target_range(1)], 'LineWidth', 3, 'Color', 'black');
+target_line2 = plot([loc_x(1) - width/2, loc_x(1) + width/2], [target_range(2) target_range(2)], 'LineWidth', 3, 'Color', 'black');
+text(loc_x(1) - width/2 - 50, target_value+2, sprintf("%d %%", target_value + margin_of_error), 'FontSize', 20, 'HorizontalAlignment', 'center', 'Color', 'black');
+text(loc_x(1) - width/2 - 50, target_value-2, sprintf("%d %%", target_value - margin_of_error), 'FontSize', 20, 'HorizontalAlignment', 'center', 'Color', 'black');
 
 % make handles for each bar to update vGRF and AP COP data
 plot_bar = plot([loc_x(1)-width/2, loc_x(1)-width/2], [ylim(1), ylim(1) + 100], 'LineWidth', width - 10,'Color','red');
 
-height = ylim(2)*0.8;
 % make bar frame
 plot([loc_x(1)-width/2 loc_x(1)+width/2],[height height],'k', 'linewidth',1) % top
 plot([loc_x(1)-width/2 loc_x(1)-width/2],[ylim(1) height],'k', 'linewidth',1); % left
 plot([loc_x(1)+width/2 loc_x(1)+width/2],[ylim(1) height],'k', 'linewidth',1); % right
-
-target_value = bodyweight_N * 0.2; % 120% for body weight
-target_line = plot([loc_x(1)-50 loc_x(1)+50], [target_value target_value], 'LineWidth', 10, 'Color', 'black');
-    text(centerpoint(1), target_value+20, ['target value: ', num2str(round(target_value, 2)), ' N'], ...
-        'FontSize', 20, 'HorizontalAlignment', 'center', 'Color', 'black');
 
 %draw force plate line
 plot([0 0],get(gca,'ylim'),'k', 'linewidth',3)
@@ -73,6 +94,8 @@ plot([centerpoint(1) centerpoint(1)],get(gca,'ylim'),'k', 'linewidth',3)
 plot(get(gca,'xlim'),[ylim(2) ylim(2)],'k', 'linewidth',3)
 plot(get(gca,'xlim'),[ylim(1) ylim(1)],'k', 'linewidth',3)
 title('Left                                                            Right','fontsize',30)
+
+grf_list = [];
 
 while true
     %use event function to avoid crash
@@ -85,22 +108,57 @@ while true
             break
         end
 
-        if isempty(force{2,1}) || isempty(force{2,2})%error occurs when getting realtime grf data. Sometimes there is no data.
+        % error occurs when getting realtime grf data. Sometimes there is no data.
+        if isempty(force{2,1}) || isempty(force{2,2})
             continue
         end
         
-        GRF1 = abs(force{2,2}(1,3));%get GRF Z from plate 1
-        GRF2 = abs(force{2,1}(1,3));%get GRF Z from plate 2 unit: kgf
+        %get GRF Z from plate 1, 2 unit: kgf
+        GRF1 = abs(force{2,2}(1,3));
+        GRF2 = abs(force{2,1}(1,3));
 
-        GRF_diff = abs(GRF1 - GRF2);
+        relative_diff = abs(GRF2 - GRF1) / (GRF1 + GRF2) * 100;
 
         %Update each bar and COP line
-        set(plot_bar,'xdata',[loc_x(1) loc_x(1)],'ydata',[ylim(1) GRF_diff])
+        set(plot_bar, 'xdata', [loc_x(1), loc_x(1)],'ydata', [ylim(1), relative_diff])
         
         drawnow;
+
+        grf_list = [grf_list, relative_diff];
 
     catch exception
         disp(exception.message);
         break
     end
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% draw the graph
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   
+n = length(grf_list);
+
+[numRows, numCols] = size(grf_list);
+
+set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 1, 0.96]);
+
+hold on;
+
+title(sprintf('Percent Difference from Target Value plate (%s)', option), 'FontSize', 20);
+xlabel('Time ', 'FontSize', 15);
+ylabel('Difference ', 'FontSize', 15);
+grid on;
+
+plot((1: numCols), grf_list, 'black');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% calculate RMSE(Root Mean Sqaure Error)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+text_position_x = round(numCols / 2);
+center_rmse = sqrt(sum((grf_list - target_value).^2) / n);
+disp(['Center Mean Percent Difference: ', num2str(center_rmse), '%']);
+plot([1 numCols], [target_value target_value], ...
+    'black','LineWidth', 1, 'LineStyle','--');
+% 센터 평균 퍼센트 차이 텍스트 추가
+center_text_position_y = target_value+10;
+text(text_position_x, center_text_position_y, ['RMSE: ', num2str(center_rmse), '%'], ...
+    'FontSize', 15, 'HorizontalAlignment', 'center', 'Color', 'black');
